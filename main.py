1dd11d9beaf46570f97ef0de16bd9c96df4bfd01
Deepak Tripathy <79755187+Deepakktripathy@users.noreply.github.com>
2025-11-25 14:08:46 +0300

    Updated main.py
    Refactored memory profiling function and added new function to create lists of varying sizes.

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
 from memory_profiler import profile
 
-@profile(stream=open('memory_profile.log', 'w'))
-def something():
-    large_list = []
-    
-    # Appending elements
-    for i in range(10_000):
-        large_list.append(f"element_{i}_" + "x" * 10)  
-    
-    #  Reading elements  
-    read_count = 0
-    for i in range(5000):
-        value = large_list[i]  
-        read_count += 1
+log_file = open('memory_profile.log', 'w')
+@profile(stream=log_file)
 
-     # NEW operation: Modify some elements
-   
-    for i in range(500):
-        large_list[i] = f"modified_{i}_" + "z" * 5
-    
-    total_memory_estimate = len(large_list) * 8  # bytes per integer
-    print(f"List statistics: {len(large_list)} elements, ~{total_memory_estimate} bytes estimated")
 
-    return len(large_list)
+def ex(listsize):
+    mylist = ['hi'] * listsize
+    mylist2 = ['hello'] * (listsize * 2)
+    del mylist2
+    return mylist
 
-if __name__ == "__main__":
-    final_size = something()
-    
-    print(f" Final list size: {final_size} elements")
-    print(f"Check logs in {memory_profile.log}")
 
+print(ex(10))
+print(ex(100))
+print(ex(1000))
